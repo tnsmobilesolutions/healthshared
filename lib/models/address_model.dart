f6f61a444363793d1cfbc0ee0b7b3ea1bdcf4aed
Atanu-Sabyasachi <jena.atanusabyasachi@gmail.com>
2022-08-13 11:26:58 +0530

    longitude and latitude are added in address model

--- lib/models/address_model.dart
+++ lib/models/address_model.dart
@@ -4,15 +4,17 @@ import 'package:equatable/equatable.dart';
 
 class Address extends Equatable {
   const Address({
-    this.patientName,
     this.addressId,
     this.addressLine1,
     this.addressLine2,
     this.addressType,
     this.city,
-    this.state,
-    this.pincode,
+    this.patientName,
     this.phoneNumber,
+    this.pincode,
+    this.state,
+    this.latitude,
+    this.longitude,
   });
 
   factory Address.fromJson(String source) =>
@@ -20,15 +22,17 @@ class Address extends Equatable {
 
   factory Address.fromMap(Map<String, dynamic> map) {
     return Address(
-      patientName: map['patientName'],
       addressId: map['addressId'],
       addressLine1: map['addressLine1'],
       addressLine2: map['addressLine2'],
       addressType: map['addressType'],
       city: map['city'],
-      state: map['state'],
-      pincode: map['pincode']?.toInt(),
+      patientName: map['patientName'],
       phoneNumber: map['phoneNumber']?.toInt(),
+      pincode: map['pincode']?.toInt(),
+      state: map['state'],
+      latitude: map['latitude']?.toDouble(),
+      longitude: map['longitude']?.toDouble(),
     );
   }
 
@@ -41,6 +45,8 @@ class Address extends Equatable {
   final int? phoneNumber;
   final int? pincode;
   final String? state;
+  final double? latitude;
+  final double? longitude;
 
   @override
   bool operator ==(Object other) {
@@ -73,79 +79,69 @@ class Address extends Equatable {
 
   @override
   String toString() {
-    return 'Address(patientName: $patientName, addressId: $addressId, addressLine1: $addressLine1, addressLine2: $addressLine2, addressType: $addressType, city: $city, state: $state, pincode: $pincode, phoneNumber: $phoneNumber)';
+    return 'Address(addressId: $addressId, addressLine1: $addressLine1, addressLine2: $addressLine2, addressType: $addressType, city: $city, patientName: $patientName, phoneNumber: $phoneNumber, pincode: $pincode, state: $state, latitude: $latitude, longitude: $longitude)';
   }
 
   Address copyWith({
-    String? patientName,
     String? addressId,
     String? addressLine1,
     String? addressLine2,
     String? addressType,
     String? city,
-    String? state,
-    int? pincode,
+    String? patientName,
     int? phoneNumber,
+    int? pincode,
+    String? state,
+    double? latitude,
+    double? longitude,
   }) {
     return Address(
-      patientName: patientName ?? this.patientName,
       addressId: addressId ?? this.addressId,
       addressLine1: addressLine1 ?? this.addressLine1,
       addressLine2: addressLine2 ?? this.addressLine2,
       addressType: addressType ?? this.addressType,
       city: city ?? this.city,
-      state: state ?? this.state,
-      pincode: pincode ?? this.pincode,
+      patientName: patientName ?? this.patientName,
       phoneNumber: phoneNumber ?? this.phoneNumber,
+      pincode: pincode ?? this.pincode,
+      state: state ?? this.state,
+      latitude: latitude ?? this.latitude,
+      longitude: longitude ?? this.longitude,
     );
   }
 
   Map<String, dynamic> toMap() {
-    final result = <String, dynamic>{};
-
-    if (patientName != null) {
-      result.addAll({'patientName': patientName});
-    }
-    if (addressId != null) {
-      result.addAll({'addressId': addressId});
-    }
-    if (addressLine1 != null) {
-      result.addAll({'addressLine1': addressLine1});
-    }
-    if (addressLine2 != null) {
-      result.addAll({'addressLine2': addressLine2});
-    }
-    if (addressType != null) {
-      result.addAll({'addressType': addressType});
-    }
-    if (city != null) {
-      result.addAll({'city': city});
-    }
-    if (state != null) {
-      result.addAll({'state': state});
-    }
-    if (pincode != null) {
-      result.addAll({'pincode': pincode});
-    }
-    if (phoneNumber != null) {
-      result.addAll({'phoneNumber': phoneNumber});
-    }
-
-    return result;
+    return {
+      'addressId': addressId,
+      'addressLine1': addressLine1,
+      'addressLine2': addressLine2,
+      'addressType': addressType,
+      'city': city,
+      'patientName': patientName,
+      'phoneNumber': phoneNumber,
+      'pincode': pincode,
+      'state': state,
+      'latitude': latitude,
+      'longitude': longitude,
+    };
   }
 
   String toJson() => json.encode(toMap());
 
   @override
-  List<Object?> get props => [
-        addressId,
-        addressLine1,
-        addressLine2,
-        addressType,
-        city,
-        patientName,
-        phoneNumber,
-        pincode,
-        state,
-      ];
+  List<Object> get props {
+    return [
+      // addressId,
+      // addressLine1,
+      // addressLine2,
+      // addressType,
+      // city,
+      // patientName,
+      // phoneNumber,
+      // pincode,
+      // state,
+      // latitude,
+      // longitude,
+    ];
+  }
 }
