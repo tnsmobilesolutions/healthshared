a42527b7eeb959004c63a0fc914c15020bb94aa3
Atanu-Sabyasachi <jena.atanusabyasachi@gmail.com>
2022-09-21 17:26:53 +0530

    DateTime converted to List<dynamic>

--- lib/models/appointments.dart
+++ lib/models/appointments.dart
@@ -1,42 +1,38 @@
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
-
 class Appointments {
   Appointments({
-    this.bookedByUser,
-    this.cancellationReason,
-    this.cancelledBy,
     this.doctorId,
     this.isAvailable,
     this.isBooked,
-    this.isCancelled,
-    required this.slotDateTime,
     this.slotId,
+    required this.slotDateTime,
+    this.isCancelled,
+    this.cancellationReason,
+    this.cancelledBy,
+    this.bookedByUser,
   });
 
   factory Appointments.fromJson(String source) =>
       Appointments.fromMap(json.decode(source));
 
   factory Appointments.fromMap(Map<String, dynamic> map) {
-    List<DateTime?>? slotDateTime;
+    DateTime? slotDateTime;
     if (map['slotDateTime'] != null) {
-      List<dynamic> slotDateTimeTimeStamp = [map['slotDateTime']];
-      slotDateTime = [
-        DateTime.fromMillisecondsSinceEpoch(
-            slotDateTimeTimeStamp[0].seconds * 1000)
-      ];
+      List<dynamic> slotDateTimeTimeStamp = map['slotDateTime'];
+      slotDateTime = DateTime.fromMillisecondsSinceEpoch(
+          slotDateTimeTimeStamp[0].seconds * 1000);
     }
     return Appointments(
-      bookedByUser: map['bookedByUser'],
-      cancellationReason: map['cancellationReason'],
-      cancelledBy: map['cancelledBy'],
       doctorId: map['doctorId'],
       isAvailable: map['isAvailable'],
       isBooked: map['isBooked'],
-      isCancelled: map['isCancelled'],
-      slotDateTime: slotDateTime,
       slotId: map['slotId'],
+      slotDateTime: slotDateTime,
+      isCancelled: map['isCancelled'],
+      cancellationReason: map['cancellationReason'],
+      cancelledBy: map['cancelledBy'],
+      bookedByUser: map['bookedByUser'],
     );
   }
 
@@ -47,7 +43,7 @@ class Appointments {
   final bool? isAvailable;
   final bool? isBooked;
   final bool? isCancelled;
-  final List<DateTime?>? slotDateTime;
+  final DateTime? slotDateTime;
   final String? slotId;
 
   @override
@@ -55,70 +51,70 @@ class Appointments {
     if (identical(this, other)) return true;
 
     return other is Appointments &&
-        other.bookedByUser == bookedByUser &&
-        other.cancellationReason == cancellationReason &&
-        other.cancelledBy == cancelledBy &&
         other.doctorId == doctorId &&
         other.isAvailable == isAvailable &&
         other.isBooked == isBooked &&
+        other.slotId == slotId &&
+        other.slotDateTime == slotDateTime &&
         other.isCancelled == isCancelled &&
-        listEquals(other.slotDateTime, slotDateTime) &&
-        other.slotId == slotId;
+        other.cancellationReason == cancellationReason &&
+        other.cancelledBy == cancelledBy &&
+        other.bookedByUser == bookedByUser;
   }
 
   @override
   int get hashCode {
-    return bookedByUser.hashCode ^
-        cancellationReason.hashCode ^
-        cancelledBy.hashCode ^
-        doctorId.hashCode ^
+    return doctorId.hashCode ^
         isAvailable.hashCode ^
         isBooked.hashCode ^
-        isCancelled.hashCode ^
+        slotId.hashCode ^
         slotDateTime.hashCode ^
-        slotId.hashCode;
+        isCancelled.hashCode ^
+        cancellationReason.hashCode ^
+        cancelledBy.hashCode ^
+        bookedByUser.hashCode;
   }
 
   @override
   String toString() {
-    return 'Appointments(bookedByUser: $bookedByUser, cancellationReason: $cancellationReason, cancelledBy: $cancelledBy, doctorId: $doctorId, isAvailable: $isAvailable, isBooked: $isBooked, isCancelled: $isCancelled, slotDateTime: $slotDateTime, slotId: $slotId)';
+    return 'Appointments(doctorId: $doctorId, isAvailable: $isAvailable, isBooked: $isBooked, slotId: $slotId, slotDateTime: $slotDateTime, isCancelled: $isCancelled, cancellationReason: $cancellationReason, cancelledBy: $cancelledBy, bookedByUser: $bookedByUser)';
   }
 
   Appointments copyWith({
-    String? bookedByUser,
-    String? cancellationReason,
-    String? cancelledBy,
     String? doctorId,
     bool? isAvailable,
     bool? isBooked,
-    bool? isCancelled,
-    List<DateTime?>? slotDateTime,
     String? slotId,
+    DateTime? slotDateTime,
+    bool? isCancelled,
+    String? cancellationReason,
+    String? cancelledBy,
+    String? bookedByUser,
   }) {
     return Appointments(
-      bookedByUser: bookedByUser ?? this.bookedByUser,
-      cancellationReason: cancellationReason ?? this.cancellationReason,
-      cancelledBy: cancelledBy ?? this.cancelledBy,
       doctorId: doctorId ?? this.doctorId,
       isAvailable: isAvailable ?? this.isAvailable,
       isBooked: isBooked ?? this.isBooked,
-      isCancelled: isCancelled ?? this.isCancelled,
-      slotDateTime: slotDateTime ?? this.slotDateTime,
       slotId: slotId ?? this.slotId,
+      slotDateTime: slotDateTime ?? this.slotDateTime,
+      isCancelled: isCancelled ?? this.isCancelled,
+      cancellationReason: cancellationReason ?? this.cancellationReason,
+      cancelledBy: cancelledBy ?? this.cancelledBy,
+      bookedByUser: bookedByUser ?? this.bookedByUser,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
-      'bookedByUser': bookedByUser,
-      'cancellationReason': cancellationReason,
-      'cancelledBy': cancelledBy,
       'doctorId': doctorId,
       'isAvailable': isAvailable,
       'isBooked': isBooked,
-      'isCancelled': isCancelled,
-      'slotDateTime': slotDateTime,
       'slotId': slotId,
+      'slotDateTime': slotDateTime?.millisecondsSinceEpoch,
+      'isCancelled': isCancelled,
+      'cancellationReason': cancellationReason,
+      'cancelledBy': cancelledBy,
+      'bookedByUser': bookedByUser,
     };
   }
 
